2b66c2aa56f8aa7afbd5c588334a2ecc5df8d895
unknown <ilkin.bos@mail.ru>
2022-08-12 21:25:53 +0400

    first commit

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from enum import unique
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique= True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
+
+    group = relationship("Group")
+
+
+class Group(Base):
+    __tablename__ = "group"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique= True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
